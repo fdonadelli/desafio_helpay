ecd55a083f46f31f2ff51feeecaee6f9323bc4d0
Fabio Donadelli <f.donaadelli@gmail.com>
2020-08-22 11:56:17 -0300

    adicionado link no email

--- resources/views/emails/body.blade.php
+++ resources/views/emails/body.blade.php
@@ -2,7 +2,7 @@
     <body>
         <p>Olá </p>
         <p></p>
-        <p>segue url : {{ $url }}</p>
+        <p>segue url : <a href={{ $url }}>{{ $url }} </a></p>
         <p></p>
         <p>Att, <br>
         Fabio Donadelli</p>
